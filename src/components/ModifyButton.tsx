c451f59e00ba67db593c71e42ee1be6eb401de7a
Angie <angeliki.iliadi1@gmail.com>
2025-04-23 22:28:17 +0200

    Modify button to change first shape item

--- src/components/ModifyButton.tsx
+++ src/components/ModifyButton.tsx
@@ -1,37 +1,37 @@
 'use client';
 
 import { Button } from "./ui/button";
-import { getSnapshot, useEditor } from "tldraw";
+import { useEditor } from "tldraw";
 import type { TLGeoShape } from 'tldraw';
 
 const ModifyButton = () => {
     const editor = useEditor();
 
     const handleClick = () => {
-        const snapshot = getSnapshot(editor.store);
-        // console.log("Snapshot at click: ", snapshot);
         const shapes = editor.getCurrentPageShapes();
         if (shapes.length === 0) return;
 
-        const shape = shapes[0];
+        const geoShape = shapes.find((shape) => shape.type === "geo") as TLGeoShape || undefined;
 
-        if (shape?.type === 'geo') {
-            const geoShape = shape as TLGeoShape & { props: { w: number; h: number; }; };
-
-            editor.updateShapes([
-                {
-                    id: geoShape.id,
-                    type: geoShape.type,
-                    x: geoShape.x + 50,
-                    y: geoShape.y,
-                    props: {
-                        ...geoShape.props,
-                        w: geoShape.props.w + 50,
-                    },
-                },
-            ]);
+        if (!geoShape) {
+            console.log("No shapes found to modify.");
+            return;
         }
+
+        editor.updateShapes([
+            {
+                id: geoShape.id,
+                type: geoShape.type,
+                x: geoShape.x + 50,
+                y: geoShape.y,
+                props: {
+                    ...geoShape.props,
+                    w: geoShape.props.w + 50,
+                },
+            },
+        ]);
     };
+
 
     return (
         <Button onClick={handleClick} variant="secondary" className="cursor-pointer"> Modify Shape</Button>
